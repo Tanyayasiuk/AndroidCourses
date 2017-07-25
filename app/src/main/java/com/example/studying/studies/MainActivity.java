3066b15f102cab08f9294917ce5b0ff764a8ec15
TanyaYasiuk <t.yasiuk@gmail.com>
2017-07-25 16:41:25 +0300

    dz1 (1st try)
    first try

--- app/src/main/java/com/example/studying/studies/MainActivity.java
+++ app/src/main/java/com/example/studying/studies/MainActivity.java
@@ -3,13 +3,84 @@ package com.example.studying.studies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{ //variant #1 for click
+
+    private Button mixButton;
+    private TextView text1;
+    private TextView text2;
+
+    //variant #2 for click
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mix();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "OnCreate()");
+
+        mixButton = (Button) findViewById(R.id.mix_button);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+
+        text1.setOnClickListener(listener);
+        text2.setOnClickListener(this);
+
+        //variant #3 for click
+        mixButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mix();
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+        mix();
+    }
+
+    public void mix(){
+        String tempText = (String) text1.getText();
+        text1.setText(text2.getText());
+        text2.setText(tempText);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    //Когда разворачивается приложение
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    //Запускается, когда сворачивается приложение
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
